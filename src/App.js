b08ea63e2b5af08a7265da8d5a9fbde9ad6df351
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-06-17 09:45:52 -0300

    campo de texto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import Banner from './componentes/Banner/Banner';
+import Banner from './componentes/Banner';
+import CampoTexto from './componentes/CampoTexto';
 
 function App() {
   return (
     <div>
       <Banner/>
-      olá react meu brother
+      <CampoTexto label="Nome" placeHolder="Digite seu nome"/>
+      <CampoTexto label="Cargo"  placeHolder="Digite seu cargo"/>
+      <CampoTexto label="Imagem"  placeHolder="Digite o endereço da sua imagem"/>
+      <CampoTexto label="Time"  placeHolder="Digite seu time"/>
     </div>
   );
 }
---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,12 @@
+import './CampoTexto.css'
+
+const CampoTexto = ({label, placeHolder}) => {
+  return (
+    <div className="campo-texto">
+      <label>{label}</label>
+      <input placeholder={placeHolder}/>
+    </div>
+  );
+}
+ 
+export default CampoTexto;
