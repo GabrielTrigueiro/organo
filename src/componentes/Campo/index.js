c63fa9a8c7da95c3ba79224a1a69caa58fba1bd6
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-06-22 18:55:15 -0300

    refatorando campo

---
+++ src/componentes/Campo/index.js
@@ -0,0 +1,34 @@
+import './Campo.css'
+
+const CampoTexto = ({
+    placeholder,
+    aoAlterado,
+    label,
+    type = 'text',
+    valor,
+    obrigatorio
+}) => {
+
+    const placeholderModificada = `${placeholder}...`
+
+    const aoDigitado = (evento) => {
+        aoAlterado(evento.target.value)
+    }
+
+    return (
+        <div className={`campo campo-${type}`}>
+            <label>
+                {label}
+            </label>
+            <input
+                type={type}
+                value={valor}
+                onChange={aoDigitado}
+                required={obrigatorio}
+                placeholder={placeholderModificada}
+            />
+        </div>
+    )
+}
+
+export default CampoTexto
